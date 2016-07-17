f1282b1c8230c9b912dce47efbd92051c67b5011
Dmitry Tarantin <dimichspb@gmail.com>
2016-07-17 18:12:04 +0300

    Remove old js functions

--- frontend/web/js/init.js
+++ frontend/web/js/init.js
@@ -1,31 +1,5 @@
 $(document).ready(function() {
-
     ymaps.ready(init);
-
-
-    var apiUrl1 = "http://www.travelpayouts.com/whereami";
-    var apiUrl2 = "http://api.biletracker.com/v1/locations";
-/*
-    $.ajax({
-        url: apiUrl1,
-        data: {},
-        dataType: "json",
-        success: function (data1) {
-            console.log(data1)
-            $.ajax({
-                url: apiUrl2,
-                data: {
-                    'iata': data1.iata
-                },
-                dataType: "json",
-                success: function (data2) {
-                    console.log(data2)
-                    $("#origin-input").typeahead('val',data1.name);
-                    $("#request-origin").val(data2.id);
-                }
-            });
-        }
-    });*/
 });
 
 function init() {

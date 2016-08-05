a48271aaa12926f6d293b32ed4e9fd5b3ea388d4
Dmitry Tarantin <dimichspb@gmail.com>
2016-08-05 16:28:49 +0300

    Fix Mobile view background issue

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,6 +1,7 @@
 <?php
 
 /* @var $this yii\web\View */
+/* @var $model \common\models\Request */
 
 use kartik\widgets\Typeahead;
 use kartik\widgets\ActiveForm;
@@ -22,7 +23,7 @@ $this->title = Yii::$app->name;
         </div>
     </div>
     <div class="row">
-        <div class="col-md-6 col-sm-12">
+        <div class="col-xs-12 col-md-6">
             <?= '<label class="control-label">'.Yii::t('app', 'Origin').'</label>'; ?>
             <?= Typeahead::widget([
                     'id' => 'origin-input',
@@ -53,7 +54,7 @@ $this->title = Yii::$app->name;
             ?>
             <?= $form->field($model, 'origin')->hiddenInput()->label(false);?>
         </div>
-        <div class="col-md-6 col-sm-12">
+        <div class="col-xs-12 col-md-6">
             <?php echo '<label class="control-label">'.Yii::t('app', 'Destination').'</label>';?>
             <?= Typeahead::widget([
                     'name' => 'destination_text',
@@ -85,55 +86,10 @@ $this->title = Yii::$app->name;
         </div>
     </div>
     <div class="row">
-        <div class="col-sm-12 col-md-6">
-            <?php
-            $layout = '
-                <span class="input-group-addon kv-date-calendar hidden-xs" title="Select date"><i class="glyphicon glyphicon-calendar"></i></span>
-                {input}';
-
-            echo FieldRange::widget([
-                'form' => $form,
-                'model' => $model,
-                'label' => Yii::t('app', 'Flight dates range'),
-                'attribute1' => 'there_start_date',
-                'attribute2' => 'there_end_date',
-                'separator' => ' - ',
-                'type' => FieldRange::INPUT_WIDGET,
-                'widgetClass' => DateControl::className(),
-                'widgetOptions1' => [
-                    'saveFormat'=>'php:Y-m-d',
-                    'displayFormat' => 'php:d.m.Y',
-                    'options'=>[
-                        'pluginOptions' => ['autoclose' => true,],
-                        'layout' => $layout,
-                    ],
-                ],
-                'widgetOptions2' => [
-                    'saveFormat'=>'php:Y-m-d',
-                    'displayFormat' => 'php:d.m.Y',
-                    'options'=>[
-                        'pluginOptions' => ['autoclose' => true,],
-                        'layout' => $layout,
-                    ],
-                ],
-            ]);
-            ?>
-        </div>
-        <div class="col-sm-12 col-md-6">
-            <?= FieldRange::widget([
-                'form' => $form,
-                'model' => $model,
-                'label' => Yii::t('app', 'Travel period range'),
-                'attribute1' => 'travel_period_start',
-                'attribute2' => 'travel_period_end',
-                'separator' => Yii::t('app', 'days'),
-                'type' => FieldRange::INPUT_SPIN,
-            ])
-            ?>
-        </div>
+        
     </div>
     <div class="row">
-        <div class="col-sm-12">
+        <div class="col-xs-12">
             <div class="form-group">
                 <?= Html::submitButton(Yii::t('app', 'Track now!'), ['class' => 'btn btn-lg btn-success btn-raised', 'name' => 'signup-button']) ?>
             </div>
